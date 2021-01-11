b1ebc36a56b8f62243f5b1d57861421823b58f02
Martin Martinez <investingmtz@gmail.com>
2021-01-11 11:44:36 -0600

    Copiando contraseña al portapapeles

--- pass_locker.py
+++ pass_locker.py
@@ -7,9 +7,16 @@ password = {
     'amazon' : 'jeffbezos'
 }
 
-import sys
+import sys, pyperclip
+
 if len(sys.argv < 2):
     print('Usage: python pass_locker.py [account] - copy account password')
     sys.exit()
 
 account = sys.argv[1]
+
+if account in password:
+    pyperclip.copy(password[account])
+    print(f'Password for {account} copied to clipboard')
+else:
+    print(f'The account {account} is not aviable')
